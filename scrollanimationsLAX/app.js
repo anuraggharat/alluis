bff9e0c955ca7189483a9a98a5a219ed822cd29b
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-06-29 00:54:55 +0530

    initial commit

---
+++ scrollanimationsLAX/app.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+	lax.setup() // init
+
+	const updateLax = () => {
+		lax.update(window.scrollY)
+		window.requestAnimationFrame(updateLax)
+	}
+
+	window.requestAnimationFrame(updateLax)
+}
